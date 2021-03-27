4db029f51fd5b21f64aaf873846df7c1741e84d6
Gustavo Benedito <gustavobeneditodeveloper@gmail.com>
2021-03-27 20:40:17 -0300

    att

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const target = document.querySelectorAll(".data-anime");
 const animationClass = 'data-anime-animate';
 
 function animeScroll(){
-    const windowTop = window.pageYOffset + (window.innerHeight*3)/4;
+    const windowTop = window.pageYOffset + (window.innerHeight*5)/6;
     target.forEach(function(element){
         if((windowTop)> element.offsetTop){
             element.classList.add(animationClass);
